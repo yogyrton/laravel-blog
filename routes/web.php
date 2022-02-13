eb2881b0ccf4c6ff47feced90fbc69b16ad99c3c
EgorParusov <parusov.93@mail.ru>
2022-02-13 18:27:54 +0300

    Create project

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function (){
+   Route::get('/', [MainController::class, 'index'])->name('admin.index');
+});
---
+++ webpack.mix.js
@@ -0,0 +1,26 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css','resources/assets/admin/css/adminlte.min.css'
+], 'public/assets/admin/css/admin.css');
+
+mix.scripts([
+   'resources/assets/admin/plugins/jquery/jquery.min.js',
+   'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+   'resources/assets/admin/js/adminlte.min.js',
+   'resources/assets/admin/js/demo.js',
+], 'public/assets/admin/js/admin.js');
+
+mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
