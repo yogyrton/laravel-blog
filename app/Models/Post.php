89fc6da7854056191fc7a77c88b4b1c478890519
EgorParusov <parusov.93@mail.ru>
2022-02-18 11:35:56 +0300

    create MainController

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        return$this->belongsTo(Category::class);
+    }
+
 }
